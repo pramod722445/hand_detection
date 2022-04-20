c5ff050907bf200ce7f371b1a683050e228631a9
Dat Nguyen <47299145+Dat89@users.noreply.github.com>
2022-04-19 22:30:42 -0800

    Added help buttons for relevant morse information

--- app/src/main/java/com/example/imagepro/MainActivity.java
+++ app/src/main/java/com/example/imagepro/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.imagepro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -23,17 +28,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Button camera_button;
+    private ImageView help_button;
+    private ImageView help_image;
+    private Dialog mDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         camera_button=findViewById(R.id.camera_button);
         camera_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        help_button = findViewById(R.id.helpBtn);
+        mDialog = new Dialog(this);
+        help_image = findViewById(R.id.morseHelp);
+        help_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mDialog.setContentView(R.layout.popup_help);
+                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                mDialog.show();
             }
         });
 
